d1de3b4604ef8082ae6dff8edd999f5412a69b3b
Maqhobela <Maqhobela.Kao@smollan.com>
2025-10-23 23:19:15 +0200

    validator on the dto using class validator package

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,7 +1,18 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString, MinLength } from "class-validator";
+
 export class CreateUserDto {
     id: number;
+    @IsString({message: 'Name must be a string'})
+    @IsNotEmpty()
+    @MinLength(3, {message: 'Name should have at least 3 characters'})
     name: string;
+
+    @IsString({message: ''})
     gender: string;
+
+    @IsEmail()
     email: string;
+
+    @IsOptional()
     isMarried: boolean;
 }
