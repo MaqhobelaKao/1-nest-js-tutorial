d02bb8ede2fbc2454e555e6ae1342f6f894feaff
Maqhobela <Maqhobela.Kao@smollan.com>
2025-10-30 12:14:58 +0200

    database anychronous connection with typeorm to postgresql database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,28 @@ import { AppService } from './app.service';
 import { UserModule } from './users/users.module';
 import { TweetModule } from './tweet/tweet.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, TweetModule, AuthModule],
+  imports: [
+    UserModule,
+    TweetModule,
+    AuthModule,
+    TypeOrmModule.forRootAsync({
+      inject: [],
+      imports: [],
+      useFactory: () => ({
+        type: 'postgres',
+        database: 'nest_js_tutorials',
+        entities: [],
+        synchronize: true,
+        host: 'localhost',
+        port: 5432,
+        username: 'evershop',
+        password: 'evershop',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
