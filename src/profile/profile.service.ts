a447a32dd4b4657233a28a98563890eac02e1dcf
Maqhobela <Maqhobela.Kao@smollan.com>
2025-10-31 14:33:09 +0200

    Fetching profiles

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Profile } from './profile.entity';
 
 @Injectable()
 export class ProfileService {
     constructor(
-        // @InjectRepository(Profile)
-        // private readonly profileRepository: Repository<Profile>
+        @InjectRepository(Profile)
+        private readonly profileRepository: Repository<Profile>
     ) {}
+
+    getAllProfiles() {
+        return this.profileRepository.find();
+    }
 }
