cc47ab6d2a601c37d8b39d7bf590e74a20823e76
Maqhobela <Maqhobela.Kao@smollan.com>
2025-11-08 11:48:52 +0200

    Module specific confuguration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,25 +1,23 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import * as config from '@nestjs/config';
+import authConfig from './config/auth.config';
 import { UsersService } from 'src/users/user.service';
+import { log } from 'console';
 
 @Injectable()
 export class AuthService {
   IsAuthenticated: boolean = false;
 
   constructor(
-    @Inject(forwardRef(()=> UsersService)) private readonly userService: UsersService,
+    @Inject(forwardRef(()=> UsersService)) 
+    private readonly userService: UsersService,
+    @Inject(authConfig.KEY)
+    private readonly authConfiguration: config.ConfigType<typeof authConfig>,
   ) {}
 
  
   logIn(email: string, password: string) {
-    const user = this.userService.getUsers().then(
-      (user) => user.find(u => u.email === email && u.password === password),
-    );
-    if (user != undefined) {
-      this.IsAuthenticated = true;
-      return { message: 'Login successful', token: 'TOKEN' };
-    } else {
-      return { message: 'Invalid email or password' };
-    }
+    log('Shared Secret:', this.authConfiguration.sharedSecret);
   }
 
   isLoggedIn() {
