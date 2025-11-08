4de4c09d1d39cd873d8546e7aadacb4cc6c428fa
Maqhobela <Maqhobela.Kao@smollan.com>
2025-11-08 14:38:12 +0200

    Handling the exception with trycatch block

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -4,12 +4,12 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { Profile } from 'src/profile/profile.entity';
+import { log } from 'console';
 
 @Injectable()
 export class UsersService {
-
   constructor(
-    @InjectRepository(User) 
+    @InjectRepository(User)
     private userRepository: Repository<User>,
 
     @InjectRepository(Profile)
@@ -24,32 +24,52 @@ export class UsersService {
         },
       });
     } catch (error) {
-      throw new RequestTimeoutException('Request timed out, please try again later.','Couldn\'t connect to database');
+      throw new RequestTimeoutException(
+        'Request timed out, please try again later.',
+        "Couldn't connect to database",
+      );
     }
   }
 
   async getUserById(id: number) {
-    return await this.userRepository.findOneBy({id: id});
+    return await this.userRepository.findOneBy({ id: id });
   }
 
-  public async createUser(userDto: CreateUserDto){
-    // Create profile if provided
+  public async createUser(userDto: CreateUserDto) {
+    try {
+      // Create profile if provided
 
-
-    // create user object
-    const user = this.userRepository.create({
-      email: userDto.email,
-      username: userDto.username,
-      password: userDto.password,
-      profile: userDto.profile ? this.profileRepository.create(userDto.profile) : {},
-    });
-    // save user to database
-    return await this.userRepository.save(user);
+      // create user object
+      const user = this.userRepository.create({
+        email: userDto.email,
+        username: userDto.username,
+        password: userDto.password,
+        profile: userDto.profile
+          ? this.profileRepository.create(userDto.profile)
+          : {},
+      });
+      // save user to database
+      return await this.userRepository.save(user);
+    } catch (error) {
+      if (error.code === 'ECONNREFUSED') {
+        throw new RequestTimeoutException(
+          'Request timed out, please try again later.',
+         {
+          description: "Couldn't connect to database",
+         }
+        );
+       
+      }
+      else if (error.code === 23505) {
+        throw new RequestTimeoutException('There is duplicate value for username',{
+          description: "Duplicate Value",
+        });
+      }
+      log(error);
+    }
   }
-
 
   public async deleteUser(id: number) {
     return await this.userRepository.delete(id);
   }
-
 }
