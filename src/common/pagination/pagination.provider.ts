e0c2cb733df91f203e654dd6223f217ee79e5dae
Maqhobela <Maqhobela.Kao@smollan.com>
2025-11-12 23:05:12 +0200

    Pagination module and added pagination to tweet service

--- src/common/pagination/pagination.provider.ts
+++ src/common/pagination/pagination.provider.ts
@@ -1,14 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import {REQUEST } from '@nestjs/core';
+import { log } from 'console';
+import express from 'express';
 import { PaginationDto } from 'src/common/pagination/dto/pagination-query.dto';
 import { FindManyOptions, FindOptionsWhere, ObjectLiteral, Repository } from 'typeorm';
+import { Paginatited } from './paginated.interface';
 
 @Injectable()
 export class PaginationProvider {
+    constructor(
+        @Inject(REQUEST) private readonly request: express.Request
+    ){}
+
+
     public async paginateQuery<T extends ObjectLiteral>(
         PaginationDto: PaginationDto,
         repository: Repository<T>,
         where? : FindOptionsWhere<T>
-    ){
+
+    ): Promise<Paginatited<T>>{
+
        const findOptions: FindManyOptions<T> = {
         skip: (PaginationDto.page! - 1) * PaginationDto.limit!,
         take: PaginationDto.limit,
@@ -17,8 +28,30 @@ export class PaginationProvider {
        if(where){
          findOptions.where = where;
        }
+        const result  =  await repository.find(findOptions); 
 
-        return await repository.find(findOptions); 
+        const totalItems = await repository.count({where});
+        const totalPages = Math.ceil(totalItems / PaginationDto.limit!);
+        const baseUrl = `${this.request.protocol}://${this.request.get('host')}`;
+        const newUrl = new URL(this.request.originalUrl, baseUrl);
+        const fullUrl = newUrl.origin + newUrl.pathname;
+
+        return {
+            data: result,
+            meta: {
+                totalItems: totalItems,
+                itemCount: result.length,
+                itemsPerPage: PaginationDto.limit!,
+                totalPages: totalPages,
+                currentPage: PaginationDto.page!,
+            },
+            links: {
+                first: `${fullUrl}?limit=${PaginationDto.limit}&page=1`,
+                previous: PaginationDto.page! > 1 ? `${fullUrl}?limit=${PaginationDto.limit}&page=${PaginationDto.page! - 1}` : null,
+                next: PaginationDto.page! < totalPages ? `${fullUrl}?limit=${PaginationDto.limit}&page=${PaginationDto.page! + 1}` : null,
+                last: `${fullUrl}?limit=${PaginationDto.limit}&page=${totalPages}`,
+            },
+        };
     }
         
 }
